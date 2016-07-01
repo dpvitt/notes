1193aacf17db31e928def1200d04de8f2ca55ef0
dpvitt <daniel@danielpavitt.com>
2016-07-01 15:57:16 +0100

    use create_note static method for creating notes in all note test cases

--- tests/test_notes.py
+++ tests/test_notes.py
@@ -22,6 +22,13 @@ class NoteTestCase(unittest.TestCase):
 
     ##### Notes Cases #####
 
+    @staticmethod
+    def create_note(self):
+        AuthTestCase.signInUser(self)
+        self.client.post(url_for('notes_route.add_tag'), data=test_mocks.tag_body)
+        self.client.post(url_for('notes_route.add_note'), data=test_mocks.note_body)
+        return self.client.get(url_for('notes_route.note', id=1))
+
     def test_notes_view_notes(self):
         AuthTestCase.signInUser(self)
         response = self.client.get(url_for('notes_route.notes'))
@@ -39,9 +46,8 @@ class NoteTestCase(unittest.TestCase):
 
     def test_notes_view_by_tag(self):
         AuthTestCase.signInUser(self)
-        self.client.post(url_for('notes_route.add_tag'), data=test_mocks.tag_body)
+        NoteTestCase.create_note(self)
         self.client.post(url_for('notes_route.add_tag'), data=test_mocks.tag_body_2)
-        self.client.post(url_for('notes_route.add_note'), data=test_mocks.note_body)
         self.client.post(url_for('notes_route.add_note'), data=test_mocks.note_body_2)
         notes = self.client.get(url_for('notes_route.notes_by_tag', id=1))
         self.assertTrue('this is an example note' in notes.get_data(as_text=True))
@@ -61,13 +67,6 @@ class NoteTestCase(unittest.TestCase):
         self.assertTrue(response.status_code == 302)
         notes = self.client.get(url_for('notes_route.notes'))
         self.assertTrue('cheese' in notes.get_data(as_text=True))
-
-    @staticmethod
-    def create_note(self):
-        AuthTestCase.signInUser(self)
-        self.client.post(url_for('notes_route.add_tag'), data=test_mocks.tag_body)
-        self.client.post(url_for('notes_route.add_note'), data=test_mocks.note_body)
-        return self.client.get(url_for('notes_route.note', id=1))
 
     def test_notes_view_note(self):
         response = NoteTestCase.create_note(self)
@@ -90,8 +89,7 @@ class NoteTestCase(unittest.TestCase):
 
     def test_notes_cannot_view_private_note(self):
         AuthTestCase.signInUser(self)
-        self.client.post(url_for('notes_route.add_tag'), data=test_mocks.tag_body)
-        self.client.post(url_for('notes_route.add_note'), data=test_mocks.note_body)
+        NoteTestCase.create_note(self)
         self.client.get(url_for('auth_route.logout'))
         response = self.client.get(url_for('notes_route.note', id=1))
         self.assertTrue(response.status_code == 403)
@@ -99,16 +97,14 @@ class NoteTestCase(unittest.TestCase):
 
     def test_notes_edit_note_content(self):
         AuthTestCase.signInUser(self)
-        self.client.post(url_for('notes_route.add_tag'), data=test_mocks.tag_body)
-        self.client.post(url_for('notes_route.add_note'), data=test_mocks.note_body)
+        NoteTestCase.create_note(self)
         response = self.client.get(url_for('notes_route.edit_note', id=1))
         self.assertTrue(response.status_code == 200)
         self.assertTrue('this is an example note</textarea>' in response.get_data(as_text=True))
 
     def test_notes_edit_note_post(self):
         AuthTestCase.signInUser(self)
-        self.client.post(url_for('notes_route.add_tag'), data=test_mocks.tag_body)
-        self.client.post(url_for('notes_route.add_note'), data=test_mocks.note_body)
+        NoteTestCase.create_note(self)
         edit = self.client.post(url_for('notes_route.edit_note', id=1), data=test_mocks.note_updated_body)
         self.assertTrue(edit.status_code == 302)
         response = self.client.get(url_for('notes_route.note', id=1))
